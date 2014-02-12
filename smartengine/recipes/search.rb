2c517e8e989c66c1a2aeb4f2f74cd0e924cc8d78
t3hpr1m3 <vmizzle@gmail.com>
2014-02-11 22:15:42 -0500

    Additional couchbase tweaks

--- smartengine/recipes/search.rb
+++ smartengine/recipes/search.rb
@@ -11,6 +11,11 @@ execute "#{node.elasticsearch[:dir]}/elasticsearch/bin/plugin -install transport
   not_if "test -d #{node.elasticsearch[:dir]}/elasticsearch/plugins/transport-couchbase"
 end
 
+execute "curl -XPUT http://localhost:9200/_template/couchbase -d @#{node.elasticsearch[:dir]}/elasticsearch/plugins/transport-couchbase/couchbase_template.json" do
+  user 'root'
+  not_if "test -d #{node.elasticsearch[:dir]}/config/templates/couchbase_template.json"
+end
+
 execute "#{node.elasticsearch[:dir]}/elasticsearch/bin/plugin -install elasticsearch/elasticsearch-analysis-kuromoji/1.7.0" do
   user 'root'
   not_if "test -d #{node.elasticsearch[:dir]}/elasticsearch/plugins/analysis-kuromoji"
